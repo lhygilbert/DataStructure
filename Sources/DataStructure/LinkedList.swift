9edeaaa23f9d2c6d9b206adc2c205c0b8487501a
Gilbert Lo <lhygilbert@gmail.com>
2020-04-26 17:57:13 -0700

    Add LinkedList

---
+++ Sources/DataStructure/LinkedList.swift
@@ -0,0 +1,196 @@
+//
+//  LinkedList.swift
+//  
+//
+//  Created by Gilbert Lo on 4/26/20.
+//
+
+import Foundation
+
+public final class LinkedList<Element> {
+    
+    private class Node<Element> {
+        let item: Element?
+        var next: Node<Element>?
+        weak var prev: Node<Element>?
+        
+        init(item: Element?, next: Node<Element>? = nil, prev: Node<Element>? = nil) {
+            self.item = item
+            self.next = next
+            self.prev = prev
+        }
+    }
+    
+    private let sentinel: Node<Element>
+    private var size: Int
+    
+    /// The number of element in the list
+    public var count: Int {
+        return size
+    }
+    
+    /// A Boolean value indicating whether the heap is empty.
+    public var isEmpty: Bool {
+        size == 0
+    }
+    
+    /// The first element of the list
+    public var first: Element? {
+        return sentinel.next?.item
+    }
+    
+    /// The last element of the list
+    public var last: Element? {
+        return sentinel.prev?.item
+    }
+    
+    public init() {
+        size = 0
+        sentinel = Node(item: nil)
+        sentinel.next = sentinel
+        sentinel.prev = sentinel
+    }
+    
+    /// Accesses the element at the specified position, error if index out of range
+    /// - Parameter index: The position of the element to access
+    /// - Returns: The element at the specified position
+    /// - Complexity: O(n)
+    public subscript(index: Int) -> Element {
+        return node(at: index).item!
+    }
+    
+    /// Inserts a new element into the list at the specified position.
+    /// - Parameters:
+    ///   - newElment: The new element to insert into the list.
+    ///   - index: The position at which to insert the new element. index must be a valid index
+    public func insert(_ newElment: Element, at index: Int) {
+        if index == 0 {
+            appendFirst(newElment)
+        } else if index == size {
+            appendLast(newElment)
+        } else {
+            let prevNode = node(at: index - 1)
+            
+            let newNode = Node(item: newElment, next: prevNode.next, prev: prevNode)
+            prevNode.next = newNode
+            size += 1
+        }
+    }
+    
+    /// Appends the specified element at the beginning of this list.
+    /// - Parameter element: Element to be inserted at the beginning
+    public func appendFirst(_ element: Element) {
+        sentinel.next = Node(item: element, next: sentinel.next, prev: sentinel)
+        // Change the old first node.prev point to new Node
+        sentinel.next?.next?.prev = sentinel.next
+        
+        size += 1
+    }
+    
+    /// Appends the specified element to the end of this list.
+    /// - Parameter element: Element to be appended
+    public func appendLast(_ element: Element) {
+        let lastNode = sentinel.prev
+        lastNode?.next = Node(item: element, next: sentinel, prev: lastNode)
+        sentinel.prev = lastNode?.next
+        
+        size += 1
+    }
+    
+    /// Removes all the elements from the list
+    public func removeAll() {
+        sentinel.next = sentinel
+        sentinel.prev = sentinel
+        size = 0
+    }
+    
+    /// Removes and returns the elment at a specific index. Error if index out of range
+    ///
+    /// - Parameter index: Integer value of the index of the node to be removed
+    /// - Returns: The data value contained in the deleted node
+    @discardableResult public func remove(at index: Int) -> Element? {
+        let nodeToRemove = node(at: index)
+        
+        let next = nodeToRemove.next
+        let prev = nodeToRemove.prev
+        
+        prev?.next = next
+        next?.prev = prev
+        
+        size -= 1
+        return nodeToRemove.item
+    }
+    
+    /// Removes and returns the element at the front of the list
+    /// - Returns: Element at the front of the list
+    @discardableResult public func removeFirst() -> Element? {
+        return remove(at: 0)
+    }
+    
+    /// Removes and returns the element at the end of the list
+    /// - Returns: Element at the end of the list
+    @discardableResult public func removeLast() -> Element? {
+        return remove(at: size - 1)
+    }
+    
+    // MARK: - Helper functions
+    
+    /// Returns the node at a specific index. Error if index out of range
+    /// - Parameter index: Integer value of the index of the node
+    /// - Returns: The node at the specific index
+    private func node(at index: Int) -> Node<Element> {
+        guard index >= 0 && index < size else {
+            fatalError("Index out of range")
+        }
+        
+        var ptr = sentinel.next
+        for _ in 0..<index {
+            ptr = ptr?.next
+        }
+        return ptr!
+    }
+    
+}
+
+// MARK: - ExpressibleByArrayLiteral Collection
+extension LinkedList: ExpressibleByArrayLiteral {
+    public convenience init(arrayLiteral: Element...) {
+        self.init()
+        for element in arrayLiteral {
+            appendLast(element)
+        }
+    }
+}
+
+// MARK: - CustomStringConvertible Collection
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        var result = "["
+        
+        var ptr = sentinel.next
+        while let value = ptr?.item {
+            ptr = ptr?.next
+            
+            let separator = ptr === sentinel ? "" : ", "
+            result += "\(value)\(separator)"
+        }
+        
+        result.append("]")
+        return result
+    }
+}
+
+// MARK: - Collection Extension
+extension LinkedList: Collection {
+    public func index(after i: Int) -> Int {
+        i + 1
+    }
+    
+    public var startIndex: Int {
+        0
+    }
+    
+    public var endIndex: Int {
+        size
+    }
+}
---
+++ Tests/DataStructureTests/LinkedListTests.swift
@@ -0,0 +1,159 @@
+//
+//  LinkedListTests.swift
+//  
+//
+//  Created by Gilbert Lo on 4/26/20.
+//
+
+import XCTest
+@testable import DataStructure
+
+final class LinkedListTests: XCTestCase {
+    
+    func testCount() {
+        let list = LinkedList<Int>()
+        XCTAssertEqual(list.count, 0)        
+        list.appendFirst(2)
+        list.appendLast(5)
+        list.appendFirst(1)
+        list.appendLast(6)
+        XCTAssertEqual(list.count, 4)
+    }
+    
+    func testIsEmpty() {
+        let list = LinkedList<String>()
+        XCTAssert(list.isEmpty)
+        list.appendLast("str")
+        XCTAssertFalse(list.isEmpty)
+    }
+    
+    func testFirstAndLast() {
+        let list = LinkedList<Int>()
+        XCTAssertNil(list.first)
+        XCTAssertNil(list.last)
+        list.appendFirst(5)
+        list.appendLast(6)
+        XCTAssertEqual(list.first, 5)
+        XCTAssertEqual(list.last, 6)
+    }
+    
+    func testSubscript() {
+        let list = LinkedList<Int>()
+        list.appendFirst(2)
+        list.appendLast(5)
+        list.appendLast(6)
+        
+        XCTAssertEqual(list[0], 2)
+        XCTAssertEqual(list[1], 5)
+        XCTAssertEqual(list[2], 6)
+    }
+    
+    func testInsert() {
+        let list = LinkedList<Int>()
+        list.insert(3, at: 0)
+        list.insert(1, at: 0)
+        list.insert(2, at: 1)
+        XCTAssertEqual(list.first, 1)
+        XCTAssertEqual(list.last, 3)
+        XCTAssertEqual(list.count, 3)
+        
+        XCTAssertEqual(list.removeFirst(), 1)
+        XCTAssertEqual(list.first, 2)
+    }
+    
+    func testRemoveAll() {
+        let list = LinkedList<Int>()
+        list.appendFirst(2)
+        list.appendLast(5)
+        
+        list.removeAll()
+        XCTAssert(list.isEmpty)
+        XCTAssertEqual(list.count, 0)
+        
+        list.appendFirst(1)
+        XCTAssertFalse(list.isEmpty)
+        XCTAssertEqual(list.first, 1)
+    }
+    
+    func testRemove() {
+        let list = LinkedList<Int>()
+        list.appendFirst(2)
+        list.appendLast(5)
+        list.appendLast(6)
+        
+        XCTAssertEqual(list.remove(at: 1), 5)
+        XCTAssertEqual(list.first, 2)
+        XCTAssertEqual(list.last, 6)
+        XCTAssertEqual(list.count, 2)
+    }
+    
+    func testRemoveFirst() {
+        let list = LinkedList<Int>()
+        list.appendFirst(2)
+        list.appendLast(5)
+        list.appendFirst(1)
+        list.appendLast(6)
+        
+        XCTAssertEqual(list.removeFirst(), 1)
+        XCTAssertEqual(list.count, 3)
+        XCTAssertEqual(list.removeFirst(), 2)
+        XCTAssertEqual(list.count, 2)
+        XCTAssertEqual(list.removeFirst(), 5)
+        XCTAssertEqual(list.count, 1)
+        XCTAssertEqual(list.removeFirst(), 6)
+        XCTAssertEqual(list.count, 0)
+    }
+    
+    func testRemoveLast() {
+        let list = LinkedList<Int>()
+        list.appendFirst(2)
+        list.appendLast(5)
+        list.appendFirst(1)
+        list.appendLast(6)
+        
+        XCTAssertEqual(list.removeLast(), 6)
+        XCTAssertEqual(list.count, 3)
+        XCTAssertEqual(list.removeLast(), 5)
+        XCTAssertEqual(list.count, 2)
+        XCTAssertEqual(list.removeLast(), 2)
+        XCTAssertEqual(list.count, 1)
+        XCTAssertEqual(list.removeLast(), 1)
+        XCTAssertEqual(list.count, 0)
+    }
+    
+    func testExpressibleByArrayLiteral() {
+        let list: LinkedList = ["a", "b", "c"]
+        
+        XCTAssertEqual(list[0], "a")
+        XCTAssertEqual(list[1], "b")
+        XCTAssertEqual(list[2], "c")
+        XCTAssertEqual(list.first, "a")
+        XCTAssertEqual(list.last, "c")
+    }
+    
+    func testCollectionExtension() {
+        let arr = [1, 2, 3, 4, 5, 6]
+        
+        let list: LinkedList = [1, 2, 3, 4, 5, 6]
+        let testIndex = list.index(list.startIndex, offsetBy: 3)
+        XCTAssertEqual(list[testIndex], 4)
+        
+        for (index, elem) in list.enumerated() {
+            XCTAssertEqual(arr[index], elem)
+        }
+    }
+
+    static var allTests = [
+        ("testCount", testCount),
+        ("testRemoveFirst", testRemoveFirst),
+        ("testFirstAndLast", testFirstAndLast),
+        ("testSubscript", testSubscript),
+        ("testInsert", testInsert),
+        ("testRemoveAll", testRemoveAll),
+        ("testRemove", testRemove),
+        ("testRemoveFirst", testRemoveFirst),
+        ("testRemoveLast", testRemoveLast),
+        ("testExpressibleByArrayLiteral", testExpressibleByArrayLiteral),
+        ("testCollectionExtension", testCollectionExtension),
+    ]
+}
